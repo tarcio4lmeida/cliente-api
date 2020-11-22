90891f33512388eeafce0b4208a379a03f8d5095
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-11-22 17:11:18 -0300

    Testando configuracao com o banco de dados

--- src/main/java/io/github/tarcio4almeida/clientes/entity/Cliente.java
+++ src/main/java/io/github/tarcio4almeida/clientes/entity/Cliente.java
@@ -7,26 +7,33 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 @Entity
-@Getter@Setter
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-	
+
 	@Column(nullable = false, length = 150)
 	private String nome;
-	
+
 	@Column(nullable = false, length = 11)
 	private String cpf;
-	
+
 	@Column(name = "data_cadastro")
-	private LocalDate dataCadatro;
+	private LocalDate dataCadastro;
+	
+	@PrePersist
+	public void prePersist() {
+		setDataCadastro(LocalDate.now());
+	}
+
 }
