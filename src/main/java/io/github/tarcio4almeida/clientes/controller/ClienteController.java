956d4fc9dadd720027e42c244df427adf63c7bb8
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-11-22 17:42:05 -0300

    Concluindo end points

--- src/main/java/io/github/tarcio4almeida/clientes/controller/ClienteController.java
+++ src/main/java/io/github/tarcio4almeida/clientes/controller/ClienteController.java
@@ -2,28 +2,56 @@ package io.github.tarcio4almeida.clientes.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import io.github.tarcio4almeida.clientes.entity.Cliente;
 import io.github.tarcio4almeida.clientes.repositories.ClienteRepository;
 
-
 @RestController
 @RequestMapping("/api/clientes")
-public class ClienteController  {
-	
+public class ClienteController {
+
 	@Autowired
 	private ClienteRepository repository;
-	
+
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
 	public Cliente salvar(@RequestBody Cliente cliente) {
 		return repository.save(cliente);
-		
 	}
-	
+
+	@GetMapping("{id}")
+	public Cliente acharPorId(@PathVariable Integer id) {
+		return repository.findById(id)
+				.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+
+	@DeleteMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT) // cod 204
+	public void deletar(@PathVariable Integer id) {
+		repository.findById(id).map(cliente -> { // metodo map -> mapeia um objeto para fazer algo
+			repository.delete(cliente);
+			return Void.TYPE;
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+
+	@PutMapping("{id}")
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void atualizar(@PathVariable Integer id, @RequestBody @Valid Cliente clienteAtualizado) {
+		repository.findById(id).map(cliente -> {
+			cliente.setNome(clienteAtualizado.getNome());
+			cliente.setCpf(clienteAtualizado.getCpf());
+			return repository.save(cliente);
+		}).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Cliente não encontrado"));
+	}
+
 }
