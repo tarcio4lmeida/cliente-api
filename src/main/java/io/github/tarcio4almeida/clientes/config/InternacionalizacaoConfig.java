d2f0c6e318b8e31e25eac3aac1cf537cd887fa8e
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-11-22 18:02:22 -0300

    Concluindo a validação da API

--- src/main/java/io/github/tarcio4almeida/clientes/config/InternacionalizacaoConfig.java
+++ src/main/java/io/github/tarcio4almeida/clientes/config/InternacionalizacaoConfig.java
@@ -14,13 +14,13 @@ public class InternacionalizacaoConfig {
 	@Bean
 	public MessageSource messageSource() {
 		ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-		messageSource.setBasename("classpath:messages");
+		messageSource.setBasename("classpath:messages"); //raiz do classpath
 		messageSource.setDefaultEncoding("ISO-8859-1");
 		messageSource.setDefaultLocale(Locale.getDefault());
 		return messageSource;
 	}
 
-	@Bean
+	@Bean // integracao BEAN validation
 	public LocalValidatorFactoryBean validatorFactoryBean() {
 		LocalValidatorFactoryBean bean = new LocalValidatorFactoryBean();
 		bean.setValidationMessageSource(messageSource());
