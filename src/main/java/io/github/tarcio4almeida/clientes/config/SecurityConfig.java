e9aa63357944b010d9e0225d9538d3540e68c76c
tarcio almeida <tarcioalmeidalima@gmail.com>
2020-12-03 23:48:48 -0300

    adicionando as depedências e testando a configuração padrão

---
+++ src/main/java/io/github/tarcio4almeida/clientes/config/SecurityConfig.java
@@ -0,0 +1,42 @@
+package io.github.tarcio4almeida.clientes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+//    @Autowired
+//    private UsuarioService usuarioService;
+
+//    @Override
+//    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//            .userDetailsService(usuarioService)
+//            .passwordEncoder(passwordEncoder());
+//    }
+
+    @Bean
+    public AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        http
+            .csrf().disable()
+            .cors()
+        .and()
+            .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+    }
+
+//    @Bean
+//    public PasswordEncoder passwordEncoder(){
+//        return NoOpPasswordEncoder.getInstance();
+//    }
+}
